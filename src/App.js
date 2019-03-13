d2b3440f5c9ebdc1011ba74608b6af5df34aa060
Andrey E <akharitonov40px@gmail.com>
2019-03-13 18:56:00 +0500

    add touch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import Board from './logics/main';
 import './App.css';
 import Row from './components/Row';
+function getTouches(e) {
+  return e.touches
+}                                                     
+
 
 class App extends Component {
-
-    state = {
+  constructor(props){
+    super(props);
+    this.state = {
       board: [],
       size: 4,
       score: 0
     }
-    
+    this.xTouch=null;
+    this.yTouch=null;
+
+  }
+
+
   restartGame = () =>{
     Board.restartGame();
     this.updateBoard();
@@ -26,6 +36,8 @@ class App extends Component {
 
   componentDidMount(){
     window.addEventListener("keyup", this.keyHandling);
+    document.addEventListener("touchstart", this.touchStart,false);
+    document.addEventListener("touchmove", this.touchMove,false);
     Board.createBoard();
     this.updateBoard(); 
   }
@@ -51,6 +63,45 @@ class App extends Component {
       default:
         break;
     }
+  }
+
+  touchStart = (e) => {
+    e.preventDefault(); 
+    const firstTouch = getTouches(e)[0];                                      
+    this.xTouch = firstTouch.clientX;                                      
+    this.yTouch = firstTouch.clientY;   
+  }
+
+  touchMove = (e) => {
+    e.preventDefault(); 
+    if ( ! this.xTouch || ! this.yTouch ) {
+      return;
+  }
+    let xTouchEnd = e.touches[0].clientX;
+    let yTouchEnd = e.touches[0].clientY;
+    let xDiff = this.xTouch - xTouchEnd;
+    let yDiff = this.yTouch - yTouchEnd;
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+        Board.moved('left');
+        this.updateBoard();
+      } else {
+        Board.moved('right');
+        this.updateBoard();
+      }                       
+  } else {
+      if ( yDiff > 0 ) {
+        Board.moved('up');
+        this.updateBoard();
+      } else { 
+        Board.moved('down');
+        this.updateBoard();
+      }                                                                 
+  }
+
+
+    this.yTouch = null;
+    this.xTouch = null;
   }
 
   render() {
