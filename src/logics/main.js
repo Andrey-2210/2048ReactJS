131be54f1af4e7f6fc58eba768c202125130804c
Andrey E <akharitonov40px@gmail.com>
2019-03-13 15:46:32 +0500

    fix bugs

--- src/logics/main.js
+++ src/logics/main.js
@@ -1,6 +1,3 @@
-
-// const getRandomInt = (min, max) =>
-//     Math.floor(Math.random() * (max - min)) + min;
 const getRandomNumber = () =>
     Math.random() >  0.3 ? 2 : 4; 
 
@@ -31,75 +28,124 @@ class Board {
 
     moveUp() {
         let array = this.board;
-        for (let r = this.options.size - 1; r > 0; r--)
-            for (let c = 0; c < this.options.size; c++) {
-                if (array[r][c] !== 0 && array[r - 1][c] === array[r][c]) {
-                    array[r - 1][c] = array[r][c] * 2;
-                    this.score += array[r][c] * 2;
-                    array[r][c] = 0;
+        let row;
+        let isMoved = false;
+        for(let c = 0; c < this.options.size; c++)
+            for(let r = 1; r < this.options.size; r++) 
+                if(array[r][c]) {
+                    row = r;
+                    while (row - 1 >= 0) {
+                        if (!array[row - 1][c]) {
+                            array[row - 1][c] = array[row][c];
+                            array[row][c] = 0;
+                            isMoved = true;
+                            row--;
+                        } else if (array[row][c] === array[row - 1][c]) {
+                            array[row - 1][c] *= 2;
+                            this.score +=  array[row - 1][c];
+                            array[row][c] = 0;
+                            isMoved = true;
+                            break;
+                        } else
+                            break;                        
+                    }
                 }
-                if (array[r - 1][c] === 0) {
-                    array[r - 1][c] = array[r][c];
-                    array[r][c] = 0;
-                }
-            }
+            
+        this.addRandom = isMoved;
         this.board = array;
+        this.addRandomCell();
     }
 
     moveDown() {
         let array = this.board;
-        for (let r = 0; r < this.options.size - 1; r++)
-            for (let c = 0; c < this.options.size; c++) {
-                if (array[r][c] !== 0 && array[r + 1][c] === array[r][c]) {
-                    array[r + 1][c] = array[r][c] * 2;
-                    this.score += array[r][c] * 2;
-                    array[r][c] = 0;
-                }
-                if (array[r + 1][c] === 0) {
-                    array[r + 1][c] = array[r][c];
-                    array[r][c] = 0;
-                }
-            }
+        let row;
+        let isMoved = false;
+
+        for(let c = 0; c < this.options.size; c++)
+            for(let r = this.options.size - 2; r >= 0; r--) 
+                if(array[r][c]) {
+                    row = r;                   
+                    while (row + 1 < this.options.size) {
+                        if (!array[row + 1][c]) {
+                            array[row + 1][c] = array[row][c];
+                            array[row][c] = 0;
+                            isMoved = true;
+                            row++;
+                        } else if (array[row][c] === array[row + 1][c]) {
+                            array[row + 1][c] *= 2;
+                            this.score +=  array[row + 1][c];
+                            array[row][c] = 0;
+                            isMoved = true;
+                            break;
+                        } else
+                            break;                        
+                    }
+                } 
+
+        this.addRandom = isMoved;
         this.board = array;
+        this.addRandomCell();
     }
 
     moveLeft() {
         let array = this.board;
-        for (let r = 0; r < this.options.size; r++)
-            for (let c = this.options.size - 1; c > 0; c--) {
-                if (array[r][c] !== 0 && array[r][c - 1] === array[r][c]) {
-                    array[r][c - 1] = array[r][c] * 2;
-                    this.score += array[r][c] * 2;
-                    array[r][c] = 0;
+        let col;
+        let isMoved = false;
+
+        for(let r = 0; r < this.options.size; r++)
+            for(let c = 1; c < this.options.size; c++) 
+                if(array[r][c]) {
+                    col = c;
+                    while (col - 1 >= 0) {
+                        if (!array[r][col - 1]) {
+                            array[r][col - 1] = array[r][col];
+                            array[r][col] = 0;
+                            isMoved = true;
+                            col--;
+                        } else if (array[r][col] === array[r][col - 1]) {
+                            array[r][col - 1] *= 2;
+                            this.score +=  array[r][col - 1];
+                            array[r][col] = 0;
+                            isMoved = true;
+                            break;
+                        } else
+                            break;                        
+                    }
                 }
-                if (array[r][c - 1] === 0) {
-                    array[r][c - 1] = array[r][c];
-                    array[r][c] = 0;
-                }
-            }
+        this.addRandom = isMoved;       
         this.board = array;
+        this.addRandomCell();
     }
 
     moveRight() {
         let array = this.board;
-        // let moved = false;
-        for (let r = 0; r < this.options.size; r++)
-            for (let c = 0; c < this.options.size - 1; c++) {
-                if (array[r][c] !== 0 && array[r][c + 1] === array[r][c]) {
-                    array[r][c + 1] = array[r][c] * 2;
-                    this.score += array[r][c] * 2;
-                    array[r][c] = 0;
-                    // moved = true;
-                }
-                if (array[r][c + 1] === 0) {
-                    array[r][c + 1] = array[r][c];
-                    array[r][c] = 0;
-                    // moved = true;
-                }
-            }
-        // this.addRandom = moved;
-        this.board = array;
+        let col;
+        let isMoved = false;
 
+        for(let r = 0; r < this.options.size; r++)
+            for(let c = this.options.size - 2; c >= 0; c--) 
+                if(array[r][c]) {
+                    col = c;
+                    while (col + 1 < this.options.size) {
+                        if (!array[r][col + 1]) {
+                            array[r][col + 1] = array[r][col];
+                            array[r][col] = 0;
+                            isMoved = true;
+                            col++;
+                        } else if (array[r][col] === array[r][col + 1]) {
+                            array[r][col + 1] *= 2;
+                            this.score +=  array[r][col + 1];
+                            array[r][col] = 0;
+                            isMoved = true;
+                            break;
+                        } else
+                            break;                        
+                    }
+                }      
+
+        this.addRandom = isMoved;        
+        this.board = array;
+        this.addRandomCell();
     }
 
     moved(direction) {
@@ -114,12 +160,12 @@ class Board {
                     this.moveLeft();
                     break;
                 case "right":
-                    this.moveRight();
+                    this.moveRight();                    
                     break;
                 default:
                     break;
             }
-            this.addRandomCell();
+
     }
 
     getEmptyCells() {
@@ -129,18 +175,19 @@ class Board {
                 if (this.board[r][c] === 0)
                     dummyArr.push([r, c]);
         }
-        if (!dummyArr.length){
-            this.gameOver();
-        }
         return dummyArr;
     }
 
     addRandomCell() {
-        const emptyCoordinates = this.getEmptyCells();
-        if (emptyCoordinates.length){            
-            const randomCoordinate = emptyCoordinates[getRandomIntRange(emptyCoordinates.length)];
-            this.board[randomCoordinate[0]][randomCoordinate[1]] = getRandomNumber();
-        }
+        const emptyCoordinates = this.getEmptyCells();        
+        if (emptyCoordinates.length){    
+            if (this.addRandom){
+                const randomCoordinate = emptyCoordinates[getRandomIntRange(emptyCoordinates.length)];
+                this.board[randomCoordinate[0]][randomCoordinate[1]] = getRandomNumber();
+            }
+            }else{
+                this.gameOver();
+            }
     }
 
     gameOver() {
